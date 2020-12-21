51356cacd5a61e315fd13cb1e3c7015ecc687c08
Engjell18Te <45998377+Engjell18Te@users.noreply.github.com>
2020-12-21 10:44:26 +0100

    Add files via upload

---
+++ DBConnection/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace DBConnection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtFN = new System.Windows.Forms.TextBox();
+            this.txtLN = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(429, 307);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 48);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(167, 144);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(141, 179);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Firstname";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(144, 220);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Lastname";
+            this.label3.Click += new System.EventHandler(this.Label3_Click);
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(218, 141);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(100, 22);
+            this.txtID.TabIndex = 4;
+            this.txtID.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // txtFN
+            // 
+            this.txtFN.Location = new System.Drawing.Point(218, 179);
+            this.txtFN.Name = "txtFN";
+            this.txtFN.Size = new System.Drawing.Size(100, 22);
+            this.txtFN.TabIndex = 5;
+            this.txtFN.TextChanged += new System.EventHandler(this.TextBox2_TextChanged);
+            // 
+            // txtLN
+            // 
+            this.txtLN.Location = new System.Drawing.Point(220, 220);
+            this.txtLN.Name = "txtLN";
+            this.txtLN.Size = new System.Drawing.Size(100, 22);
+            this.txtLN.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtLN);
+            this.Controls.Add(this.txtFN);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.TextBox txtFN;
+        private System.Windows.Forms.TextBox txtLN;
+    }
+}
+
---
+++ DBConnection/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing; 
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace DBConnection
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            
+
+            InitializeComponent();
+        }
+       //databas länk
+        public string conString = "Data Source=CND8263QR2\\SQL2018;Initial Catalog=Prg2;Integrated Security=True";
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+            SqlConnection con = new SqlConnection(conString);   
+           //öppna länken 
+            con.Open();
+           
+            {
+                //gör en string för att hitta inskriven information 
+                string q = "Select * from Prg2.dbo.Persons";
+                //skicka länken till en kommand object
+
+                    SqlCommand cmd = new SqlCommand(q, con);
+                //får svar
+                cmd.ExecuteNonQuery();
+                //om det är rätt så kommer detta poppa up.
+                MessageBox.Show("Login made Successfuly..! ");
+            }
+        }
+
+        private void Label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+          
+        }
+    }
+}
